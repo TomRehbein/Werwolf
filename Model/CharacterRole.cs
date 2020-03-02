0d0439eb168d1e21fd999b6c4e61cf546bd54ab1
Tom Rehbein <Tom.Rehbein@Decathlon.com>
2020-03-03 00:17:43 +0100

    create simple CharacterRole Model

--- Model/CharacterRole.cs
+++ Model/CharacterRole.cs
@@ -1,12 +1,73 @@
-﻿using System;
+﻿using Werwolf.ViewModel;
+
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Werwolf.Model
 {
    class CharacterRole
    {
+      public string Name { get; set; }
+      public string Description { get; set; }
+      public bool Alive { get; set; }
+      public bool WakesUp { get; set; }
+      public uint WakeUpOrder { get; set; }
+      public Image Image { get; set; }
+
+      public CharacterRole(string name, string description, bool wakesUp, uint wakeUpOrder)
+      {
+         this.Name = name;
+         this.Description = description;
+         this.Alive = true;
+         this.WakesUp = wakesUp;
+         this.WakeUpOrder = wakeUpOrder;
+         this.Image = FindImage(name);
+      }
+
+      public CharacterRole(string name, string description, bool wakesUp)
+      {
+         this.Name = name;
+         this.Description = description;
+         this.Alive = true;
+         this.WakesUp = wakesUp;
+         this.Image = FindImage(name);
+      }
+
+      public CharacterRole(string name, bool wakesUp, uint wakeUpOrder)
+      {
+         this.Name = name;
+         this.Alive = true;
+         this.WakesUp = wakesUp;
+         this.WakeUpOrder = wakeUpOrder;
+         this.Image = FindImage(name);
+      }
+
+      public CharacterRole(string name, bool wakesUp)
+      {
+         this.Name = name;
+         this.Alive = true;
+         this.WakesUp = wakesUp;
+         this.Image = FindImage(name);
+      }
+
+      private Image FindImage(string name)
+      {
+         name = name.Replace(" ", "_");
+         if(File.Exists(ModeratorViewModel.workDirEtcPic + name + ".jpg"))
+         {
+            return Image.FromFile(ModeratorViewModel.workDirEtcPic + name + ".jpg");
+         }
+
+         return null;
+      }
+
+      public void Ability()
+      { 
+      }
    }
 }
