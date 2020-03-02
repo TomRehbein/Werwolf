7a209396b4e9d8e53b0049b0292d8a46b8ce00c7
Tom Rehbein <Tom.Rehbein@Decathlon.com>
2020-03-02 20:20:01 +0100

    MVVM set up

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace Werwolf
 {
@@ -23,6 +10,14 @@ namespace Werwolf
       public MainWindow()
       {
          InitializeComponent();
+      }
+
+      private void MainViewControl_Loaded(object sender, RoutedEventArgs e)
+      {
+         Werwolf.ViewModel.ModeratorViewModel MainViewModelObject =
+            new Werwolf.ViewModel.ModeratorViewModel();
+
+         MainViewControl.DataContext = MainViewModelObject;
       }
    }
 }
