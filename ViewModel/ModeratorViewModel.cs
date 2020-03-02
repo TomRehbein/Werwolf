fb98ff3d5c9e04f9a836f7bcdc165ca5d35ab874
Tom Rehbein <Tom.Rehbein@Decathlon.com>
2020-03-02 23:29:58 +0100

    add setup function

--- ViewModel/ModeratorViewModel.cs
+++ ViewModel/ModeratorViewModel.cs
@@ -1,12 +1,34 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+using System.Windows;
 
 namespace Werwolf.ViewModel
 {
    class ModeratorViewModel
    {
+      public static string workDir = Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData) + @"/Bundesliga";
+      public static string workDirEtc = workDir + @"/etc";
+      public static string workDirEtcPic = workDirEtc + @"/Pictures";
+
+      public ModeratorViewModel()
+      {
+         SetUp();
+      }
+
+      private void SetUp()
+      {
+         if (!Directory.Exists(workDir))
+         {
+            Directory.CreateDirectory(workDir);
+            Directory.CreateDirectory(workDirEtc);
+            //string currentDir = Directory.GetCurrentDirectory();
+            Directory.CreateDirectory(workDirEtcPic);
+         }
+      }
+
+
    }
 }
