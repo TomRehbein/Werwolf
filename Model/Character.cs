670abf09249161ea149441e4e9d7175456fd90bd
Tom Rehbein <Tom.Rehbein@Decathlon.com>
2020-03-03 16:07:25 +0100

    basic GameSetUp ViewModel done | count added to Character Model

--- Model/Character.cs
+++ Model/Character.cs
@@ -16,10 +16,11 @@ namespace Werwolf.Model
       public string Description { get; set; }
       public bool Alive { get; set; }
       public bool WakesUp { get; set; }
-      public uint WakeUpOrder { get; set; }
+      public int WakeUpOrder { get; set; }
       public Image Image { get; set; }
+      public int Count { get; set; }
 
-      public Character(string name, string description, bool wakesUp, uint wakeUpOrder)
+      public Character(string name, string description, bool wakesUp, int wakeUpOrder, int count)
       {
          this.Name = name;
          this.Description = description;
@@ -27,32 +28,36 @@ namespace Werwolf.Model
          this.WakesUp = wakesUp;
          this.WakeUpOrder = wakeUpOrder;
          this.Image = FindImage(name);
+         this.Count = count;
       }
 
-      public Character(string name, string description, bool wakesUp)
+      public Character(string name, string description, bool wakesUp, int count)
       {
          this.Name = name;
          this.Description = description;
          this.Alive = true;
          this.WakesUp = wakesUp;
          this.Image = FindImage(name);
+         this.Count = count;
       }
 
-      public Character(string name, bool wakesUp, uint wakeUpOrder)
+      public Character(string name, bool wakesUp, int wakeUpOrder, int count)
       {
          this.Name = name;
          this.Alive = true;
          this.WakesUp = wakesUp;
          this.WakeUpOrder = wakeUpOrder;
          this.Image = FindImage(name);
+         this.Count = count;
       }
 
-      public Character(string name, bool wakesUp)
+      public Character(string name, bool wakesUp, int count)
       {
          this.Name = name;
          this.Alive = true;
          this.WakesUp = wakesUp;
          this.Image = FindImage(name);
+         this.Count = count;
       }
 
       private Image FindImage(string name)
--- ViewModel/GameSetUpViewModel.cs
+++ ViewModel/GameSetUpViewModel.cs
@@ -1,12 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Werwolf.Model;
+
+using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Windows.Input;
+using System.Windows;
 
 namespace Werwolf.ViewModel
 {
    class GameSetUpViewModel
    {
+      public ObservableCollection<Character> Characters{ get; set; }
+
+
+      public MyICommand AddRoleCommand { get; set; }
+      public MyICommand DeleteRoleCommand { get; set; }
+      public RelayCommand PlayCommand { get; set; }
+
+      private Character _selectedCharacter { get; set; }
+      public Character SelectedCharacter
+      {
+         get
+         {
+            return _selectedCharacter;
+         }
+         set
+         {
+            _selectedCharacter = value;
+         }
+      }
+
+      public GameSetUpViewModel()
+      {
+
+
+         AddRoleCommand = new MyICommand(OnAdd, CanAdd);
+         DeleteRoleCommand = new MyICommand(OnDelete, CanDelete);
+         PlayCommand = new RelayCommand((parameter) => StartGame());
+      }
+
+      private void OnAdd()
+      {
+         // show a View of all remaining Roles as png
+
+         //Characters.Add(new Character());
+      }
+
+      private bool CanAdd()
+      {
+         return true;
+      }
+
+      private void OnDelete()
+      {
+         Characters.Remove(SelectedCharacter);
+      }
+
+      private bool CanDelete()
+      {
+         return SelectedCharacter != null;
+      }
+
+      private void StartGame()
+      {
+         // Close the View and hands over the Characters
+      }
    }
 }
